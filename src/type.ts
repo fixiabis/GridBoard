fe0d7a0007bf58dfa277be837a5ec2e3c4ff0c3e
fixiabis <fixiabis@gmail.com>
2020-02-23 22:50:45 +0800

    refactor

--- src/type.ts
+++ src/type.ts
@@ -1,20 +1,17 @@
 import Grid from "./Grid";
 
 /**
- * 類棋盤格
- * @typedef {object} GridLike
- * @template GridPiece 類棋盤格上面的棋子類型，預設為 "any"
- * @template GridState 類棋盤格自身的狀態類型，預設為 "any"
- * @property {GridPiece | null} piece 類棋盤格上面的棋子
- * @property {GridState} state 類棋盤格自身的狀態
+ * 也許有特定欄位的介面，若欄位值為 "never" 則該介面無此欄位
+ * @typedef InterfaceMaybeHasKey
+ * @template Interface 介面
+ * @template Key 欄位
+ * @template Value 欄位值，預設為 "never"
  */
-export interface GridLike<GridPiece = any, GridState = any> {
-    /** 類棋盤格上面的棋子 */
-    piece: GridPiece | null;
-
-    /** 類棋盤格自身的狀態 */
-    state: GridState;
-}
+export type MaybeHasKey<Interface, Key extends string, Value = never> = (
+    Value[] extends never[]
+    ? Omit<Interface, Key>
+    : Interface
+);
 
 /**
  * 棋盤快照
@@ -54,20 +51,6 @@ export interface GridSnapshot<GridPiece, GridState = never> {
 }
 
 /**
- * 也許有狀態的類棋盤格
- * @typedef {object} GridLikeMaybeHasState
- * @template GridPiece 類棋盤格上面的棋子類型
- * @template GridState 類棋盤格自身的狀態類型，預設為 "never"，若使用預設則 "state" 屬性不存在
- * @property {GridPiece | null} piece 類棋盤格上面的棋子
- * @property {GridState} state 類棋盤格自身的狀態，若 "GridState" 類型為 "never"，則該屬性不存在
- */
-export type GridLikeMaybeHasState<GridPiece, GridState = never> = (
-    GridState[] extends never[]
-    ? Omit<GridLike<GridPiece, GridState>, "state">
-    : GridLike<GridPiece, GridState>
-);
-
-/**
  * 也許有狀態的棋盤格
  * @typedef {object} GridMaybeHasState
  * @template GridPiece 棋盤格上面的棋子類型
@@ -76,9 +59,7 @@ export type GridLikeMaybeHasState<GridPiece, GridState = never> = (
  * @property {GridState} state 棋盤格自身的狀態，若 "GridState" 類型為 "never"，則該屬性不存在
  */
 export type GridMaybeHasState<GridPiece, GridState = never> = (
-    GridState[] extends never[]
-    ? Omit<Grid<GridPiece, GridState>, "state">
-    : Grid<GridPiece, GridState>
+    MaybeHasKey<Grid<GridPiece, GridState>, "state", GridState>
 );
 
 /**
@@ -90,7 +71,5 @@ export type GridMaybeHasState<GridPiece, GridState = never> = (
  * @property {GridState} state 棋盤格快照自身的狀態，若 "GridState" 類型為 "never"，則該屬性不存在
  */
 export type GridSnapshotMaybeHasState<GridPiece, GridState = never> = (
-    GridState[] extends never[]
-    ? Omit<GridSnapshot<GridPiece, GridState>, "state">
-    : GridSnapshot<GridPiece, GridState>
+    MaybeHasKey<GridSnapshot<GridPiece, GridState>, "state", GridState>
 );
